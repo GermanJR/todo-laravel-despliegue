c2b3fec43cfb24a48c7fb67629247a51a7a88948
GermanJR <wambows@gmail.com>
2023-12-18 17:42:30 +0100

    [ADD]

---
+++ deploy.php
@@ -0,0 +1,41 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Project name
+set('application', 'tonet_german_ud4_a4');
+
+// Project repository
+set('repository', 'https://github.com/GermanJR/todo-laravel-despliegue.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true); 
+
+// Shared files/dirs between deploys 
+add('shared_files', [".env"]);
+add('shared_dirs', ["bootstrap", "storage"]);
+
+// Writable dirs by web server 
+add('writable_dirs', ["bootstrap", "storage"]);
+
+
+// Hosts
+
+host('172.16.221.168') ->user('prod-ud4-deployer')
+ ->identityFile('~/.ssh/id_rsa')
+ ->set('deploy_path', '/var/www/prod-ud4-a4/html');    
+    
+// Tasks
+
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+
+before('deploy:symlink', 'artisan:migrate');
+
